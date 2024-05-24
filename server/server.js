de77c9de47ff73506bcde8776169ddaef2337eb7
FriedLikeRice <jacobleeferraro@gmail.com>
2024-05-23 19:39:08 -0700

    okherewego

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schema');
 const { authMiddleware } = require('./auth');
-
+''
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
