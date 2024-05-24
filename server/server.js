a62f0ebd0ce9fa261e720f0e51ddf57a33174871
FriedLikeRice <jacobleeferraro@gmail.com>
2024-05-23 19:17:10 -0700

    godplz

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { typeDefs, resolvers } = require('./schema');
 const { authMiddleware } = require('./auth');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
